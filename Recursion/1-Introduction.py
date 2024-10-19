daa763feeb3bca1165dd2b10b21b79947b88639c
Nabin Katwal <78csi26@texasintl.edu.np>
2024-10-19 08:03:30 +0545

    commit

---
+++ Recursion/1-Introduction.py
@@ -0,0 +1,27 @@
+def printNumFive(num):
+    print(num)
+    printNumFour(num-1)
+
+def printNumFour(num):
+    print(num)
+    printNumThree(num-1)
+
+def printNumThree(num):
+    print(num)
+    printNumTwo(num-1)
+
+def printNumTwo(num):
+    print(num)
+    printNumOne(num-1)
+
+def printNumOne(num):
+    print(num)
+
+
+
+printNumFive(5) 
+
+
+
+## To just use above function and a single print statement to print out numbers 5 to 1
+
