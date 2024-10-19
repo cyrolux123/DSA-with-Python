3eaf7dcd314c77ecafc2c365ae2231a0e8eb809b
Nabin Katwal <78csi26@texasintl.edu.np>
2024-10-19 10:26:35 +0545

    commit

---
+++ Recursion/8-HeadVsTailRecursion.py
@@ -0,0 +1,25 @@
+def factHead(n):
+    if n == 0:
+        return 1
+    
+    smallAns = factHead(n-1)
+    ans = n * smallAns
+    return ans
+
+print(factHead(5))
+
+'''
+def factTailWrong(n):
+    if n == 0:
+        return 1
+    return n * factTailWrong(n-1)
+'''
+
+def factTail(n, k = 1):
+    if n == 0:
+        return k
+    
+    k = n * k
+    return factTail(n-1, k)
+
+print(factTail(5))
